58a1608b172cb6d094ff88c6f3c04f657c3deb17
Adrian <adrianalarcon720@gmail.com>
2024-09-16 18:30:25 +0200

    aparentemente acabada2

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
         </div>
-        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-2 w-52 p-1 shadow">
+        <ul tabindex="0" class="menu menu-sm dropdown-content bg-azulBebe rounded-box z-[1] mt-2 w-52 p-1 shadow">
           <li>
             <router-link to="/" class="text-azulNavbar font-semibold transition-colors"
               exact-active-class="font-bold">
