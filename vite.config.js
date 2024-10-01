f0be46f30040341bc0e033e5f95c420c19b4bbc8
Adrian <adrianalarcon720@gmail.com>
2024-10-01 21:54:26 +0200

    mejorarndo seo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { copyFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,19 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Otras configuraciones de salida
+      }
+    },
+    // Hook para copiar archivos después de la construcción
+    onAfterBuild: () => {
+      copyFileSync('robots.txt', 'dist/robots.txt');
+      copyFileSync('sitemap.xml', 'dist/sitemap.xml');
+      copyFileSync('.htaccess', 'dist/.htaccess');
     }
   }
 })
